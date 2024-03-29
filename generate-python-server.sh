3085338717b85cc1fb9eb85d9ed0b45b56641fd5
Thomas Bonk <thomas@meandmymac.de>
2024-03-29 04:01:26 +0100

    calling the API implementation with async

--- generate-python-server.sh
+++ generate-python-server.sh
@@ -6,6 +6,7 @@ PACKAGE_VERSION="10.1.0"
 openapi-generator generate \
     -i pi_control_hub_api.yaml \
     -g python-fastapi \
+    -t ./python-fastapi-async-template \
     -o . \
     -p packageName=$PACKAGE_NAME \
     -p packageVersion=$PACKAGE_VERSION
